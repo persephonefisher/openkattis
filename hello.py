5b35eaadb365a24ac8b9c001b52baa285049ee21
Persephone Fisher <75499186+persephonefisher@users.noreply.github.com>
2020-12-04 15:36:35 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,2 +1,4 @@
-# Here I use the print() function to output the intended string.
+# Python 3.9 solution for Open Kattis problem "Hello World" by Persephone Fisher
+
+# Use the print() function to output the intended string
 print("Hello World!")
