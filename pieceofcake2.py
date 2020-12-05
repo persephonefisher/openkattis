8a55f080aaec7b85c1cdf2ed41b2ebc716555291
Persephone Fisher <75499186+persephonefisher@users.noreply.github.com>
2020-12-05 04:59:22 -0500

    Create pieceofcake2.py
    open.kattis.com/problems/pieceofcake2

---
+++ pieceofcake2.py
@@ -0,0 +1,17 @@
+# Python 3.9 solution for Open Kattis problem "Piece of Cake" by Persephone Fisher
+
+# Initialize the cake list
+cake = []
+
+# Use the input() function to accept input as a string, the string.split() method to convert the
+# input to a list, then cast all three list elements as int(egers) with the map() function. Since
+# there are three expected inputs, each element of the list can be stored directly into variables.
+n, h, v = map(int, input().split())
+
+# Append all possible combinations of the cake's two cuts/four slices
+cake.append(h * v)
+cake.append((n - h) * v)
+cake.append(h * (n - v))
+cake.append((n - h) * (n - v))
+
+print(max(cake) * 4)
