519c6a1390e956941e007d66d1498cc0f71aef57
Persephone Fisher <75499186+persephonefisher@users.noreply.github.com>
2021-02-02 17:06:17 -0500

    Add license info

--- r2.py
+++ r2.py
@@ -1,5 +1,9 @@
 # Python 3.9 solution for Open Kattis problem R2 by Persephone Fisher
 
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 # Use the input() function to get the input as a string, the string.split() method to convert the string to a list,
 # then convert the elements of the list to int(egers) with the map() function. Since there are two inputs, we can 
 # store the first element of the list as r1 and the second element as answer.
